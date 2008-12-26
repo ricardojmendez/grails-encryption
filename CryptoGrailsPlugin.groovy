c94977578cb40ce5c3c2ca96db9a6cd37c516b62
Ricardo J. Mendez <ricardo@arquetipos.co.cr>
2008-12-25 18:53:00 -0600

    Removed unnecessary TODOs

--- CryptoGrailsPlugin.groovy
+++ CryptoGrailsPlugin.groovy
@@ -13,25 +13,5 @@ Castle. This version provides classes for PGP and Blowfish, as well as a
 tool for both generating random passwords and salting / verifying salted
 passwords.
 '''
-	
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}   
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)		
-	}
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional)
-	}	                                      
-	def doWithDynamicMethods = { ctx ->
-		// TODO Implement additions to web.xml (optional)
-	}	
-	def onChange = { event ->
-		// TODO Implement code that is executed when this class plugin class is changed  
-		// the event contains: event.application and event.applicationContext objects
-	}                                                                                  
-	def onApplicationChange = { event ->
-		// TODO Implement code that is executed when any class in a GrailsApplication changes
-		// the event contain: event.source, event.application and event.applicationContext objects
-	}
+
 }
